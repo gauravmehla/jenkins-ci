08e92b3eeb771f5f52bb2cc84ab8e2179df2d89a
Gaurav Mehla <gaurav.ror007@gmail.com>
2017-08-02 17:43:59 +0530

    Title changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello from Express!!' });
+  res.render('index', { title: 'Hello from Gaurav!!' });
 });
 
 module.exports = router;
